906b5cf84f8f9f16d1b17510d132742792ce196b
lyly2112 <lyly2112@gmail.com>
2022-02-09 00:48:30 -0800

    Add app components  with routing.

--- heroes-app/src/app/heroes/heroes.component.ts
+++ heroes-app/src/app/heroes/heroes.component.ts
@@ -1,17 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css'],
+  styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
-  constructor() {}
+  heroes: Hero[] = [];
 
-  ngOnInit(): void {}
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
+  }
 }
